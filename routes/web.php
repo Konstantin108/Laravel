4a1880407bfaaf575660ff02b9f5d9ed8a6eb4de
Kostya <Kpstya@mail.ru>
2021-03-28 23:27:51 +0300

    remove change

--- routes/web.php
+++ routes/web.php
@@ -21,22 +21,3 @@ Route::get('/name/{name}', function(string $name): string
 {
     return "hello, {$name}";
 });
-
-Route::get('/welcome/{name}', function(string $name): string
-{
-    return "<h1>Добро пожаловать на сайт, {$name}</h1>";;
-});
-
-Route::get('/info', function()
-{
-    return 'Это учебный проект с курса по Laravel';
-});
-
-Route::get('/news', function()
-{
-    $news = ['первая новость', 'вторая новость'];
-
-    foreach ($news as $new){
-        echo "<h2>{$new}</h2><br>";
-    }
-});
